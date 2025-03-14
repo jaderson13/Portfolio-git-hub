5c1918360bb1209cd2f558b96a51768ce5cdbe8c
jaderson13 <jdergames@gmail.com>
2025-03-14 10:25:07 -0300

    remoção site de países

--- site de paises/script.js
+++
@@ -1,9 +0,0 @@
-function Ler_dados(funcao, filtro, campo, tipo_funcao) {
-    fetch('https://restcountries.com/v3.1/all')
-        .then(dados => dados.json())
-        .then(dados => {
-            vet_paises = dados;
-            if (tipo_funcao == 0) { funcao(filtro, campo); }
-            else{funcao();}     
-        })
-}
