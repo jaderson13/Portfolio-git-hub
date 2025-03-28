062d8332e027638e7338f7f4f14d499184778dab
jaderson13 <jdergames@gmail.com>
2025-03-28 16:23:39 -0300

    bug img

--- src/componentes/projetos/Projeto.jsx
+++ src/componentes/projetos/Projeto.jsx
@@ -13,21 +13,21 @@ export default function Projeto() {
                 <h3>Jogo da forca</h3>
                 <p>Jogo da forca com teclado virtual</p>
                 <a href="https://jaderson13.github.io/Projetos/jogo_da_forca/index.html">
-                    <img src="./src/assets/forca.png" alt="jogo da forca início" />
+                    <img src="/assets/forca.png" alt="jogo da forca início" />
                 </a>
             </div>
             <div>
                 <h3>Calculadora</h3>
                 <p>Calculadora simples feita com javascript</p>
                 <a href="https://jaderson13.github.io/Projetos/calculadora/index.html">
-                    <img id="pais" src="./src/assets/calc1.png" alt="Calculadora" />
+                    <img id="pais" src="/assets/calc1.png" alt="Calculadora" />
                 </a>
             </div>
             <div>
                 <h3>Blog online com banco de dados</h3>
                 <p>(30 segundos para carregar, hospedagem gratuita)</p>
                 <a href="https://blog-pg.onrender.com/">
-                    <img src="./src/assets/blog.png" alt="Imagem do blog" />
+                    <img src="/assets/blog.png" alt="Imagem do blog" />
                 </a>
             </div>
             
